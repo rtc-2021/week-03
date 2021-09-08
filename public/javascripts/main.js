42cbd7249bcf8252f4fc1ec35936bca73cd7928e
Karl Stolley <karl.stolley@gmail.com>
2021-09-08 15:16:45 -0500

    Display video stream in video element

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,6 +7,8 @@ const $self = {
 requestUserMedia($self.constraints);
 
 async function requestUserMedia(constraints) {
+  const video = document.querySelector('#self');
   $self.stream = await navigator.mediaDevices
     .getUserMedia(constraints);
+  video.srcObject = $self.stream;
 }
